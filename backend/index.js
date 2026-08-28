a613bf1c80301d4e1a164b68b77c599f168905cc
shauryanandwani <shaurya.7nandwani@gmail.com>
2026-08-28 15:27:07 +0200

    cors updation

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://frontend-08av.onrender.com"
+}));
+
 app.use(express.json());
 
 app.use("/todos", todoRoutes);
